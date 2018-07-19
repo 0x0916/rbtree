88567d0178261120a1c7244f0bc3b3a64522b511
Wang Long <w@laoqinren.net>
2018-07-19 17:43:56 +0800

    enhance testcases
    Signed-off-by: Wang Long <w@laoqinren.net>

--- rbtree_3/main.c
+++ rbtree_3/main.c
@@ -41,6 +41,36 @@ void erase(struct test_node *node, struct rb_root *root)
     rb_erase(&node->rb, root);
 }
 
+static int black_path_count(struct rb_node *rb) {
+	int count;
+	for (count = 0; rb; rb = rb_parent(rb))
+		count += !rb_is_red(rb);
+
+	return count;
+}
+
+static void check(int nr_nodes)
+{
+	struct rb_node *rb;
+	int count = 0, blacks = 0;
+	int prev_key = 0;
+
+	for (rb = rb_first(&root); rb; rb=rb_next(rb)) {
+		struct test_node *node = rb_entry(rb, struct test_node, rb);
+		if (node->key < prev_key)
+			printf("[WARN] node->key(%d) < prev_key(%d)\n", node->key, prev_key);
+		if (rb_is_red(rb) && (!rb_parent(rb) || rb_is_red(rb_parent(rb))))
+			printf("[WARN] two red nodes\n");
+		if (!count)
+			blacks = black_path_count(rb);
+		else if ((!rb->rb_left || !rb->rb_right) && (blacks != black_path_count(rb)))
+			printf("[WARN] black count wrongs\n");
+
+		prev_key = node->key;
+		count++;
+	}
+}
+
 void print_rbtree(struct rb_root *tree)
 {
 	struct rb_node *node;
@@ -68,15 +98,28 @@ int main(int argc, char *argv[])
 	seed = time(NULL);
 
 	for (i = 0; i < PERF_LOOPS; i++) {
-		printf("############### %d\n", i);
 		init();
 		for(j = 0; j < NODES; j++) {
 			insert(nodes + j, &root);
 		}
-		print_rbtree(&root);
+//		print_rbtree(&root);
 		for(j = 0; j < NODES; j++) {
 			erase(nodes + j, &root);
 		}
+	}
+
+	for (i = 0; i < PERF_LOOPS; i++) {
+		init();
+		for(j = 0; j < NODES; j++) {
+			check(j);
+			insert(nodes + j, &root);
+		}
+//		print_rbtree(&root);
+		for(j = 0; j < NODES; j++) {
+			check(NODES - j);
+			erase(nodes + j, &root);
+		}
+		check(0);
 	}
 	return 0;
 }
